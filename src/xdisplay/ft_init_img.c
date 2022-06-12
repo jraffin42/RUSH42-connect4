0b82f6790976a4a07d81a4dd09660c13e18f31ad
Floran MAUGUIN <fmauguin@e2r3p4.clusters.42paris.fr>
2022-06-12 20:31:36 +0200

    oubli

--- src/xdisplay/ft_init_img.c
+++ src/xdisplay/ft_init_img.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 00:13:53 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/06/12 20:24:38 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/06/12 20:31:19 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ static int	ft_push_img(t_c4 **c4, char c, int x, int y)
 		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
 			(*c4)->bg, x * WIDTH, y * HEIGHT);
 	else if (c == 'P' && (*c4)->p_color == YEL_CHAR)
-		if (!mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
-			(*c4)->top_yellow, x * WIDTH, y * HEIGHT));
+		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
+			(*c4)->top_yellow, x * WIDTH, y * HEIGHT);
 	else if (c == 'P' && (*c4)->p_color == RED_CHAR)
-		if (!mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
-			(*c4)->top_red, x * WIDTH, y * HEIGHT));
+		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
+			(*c4)->top_red, x * WIDTH, y * HEIGHT);
 	return (0);
 }
 
