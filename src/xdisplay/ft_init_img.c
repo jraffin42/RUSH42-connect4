4b5a8982b04a63b2469cc4ff4ae65d77a6312be1
Floran MAUGUIN <fmauguin@e2r3p4.clusters.42paris.fr>
2022-06-12 20:29:28 +0200

    void -> int protection

--- src/xdisplay/ft_init_img.c
+++ src/xdisplay/ft_init_img.c
@@ -6,23 +6,24 @@
 /*   By: fmauguin <fmauguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 00:13:53 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/06/12 18:55:19 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/06/12 20:24:38 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "xdisplay.h"
 
-static void	ft_push_img(t_c4 **c4, char c, int x, int y)
+static int	ft_push_img(t_c4 **c4, char c, int x, int y)
 {
 	if (c == ' ')
 		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
 			(*c4)->bg, x * WIDTH, y * HEIGHT);
 	else if (c == 'P' && (*c4)->p_color == YEL_CHAR)
-		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
-			(*c4)->top_yellow, x * WIDTH, y * HEIGHT);
+		if (!mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
+			(*c4)->top_yellow, x * WIDTH, y * HEIGHT));
 	else if (c == 'P' && (*c4)->p_color == RED_CHAR)
-		mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
-			(*c4)->top_red, x * WIDTH, y * HEIGHT);
+		if (!mlx_put_image_to_window((*c4)->mlx, (*c4)->win,
+			(*c4)->top_red, x * WIDTH, y * HEIGHT));
+	return (0);
 }
 
 int	ft_init_img(t_c4 **c4)
@@ -30,7 +31,8 @@ int	ft_init_img(t_c4 **c4)
 	int	i;
 	int	j;
 
-	ft_push_img(c4, 'P', 0, 0);
+	if (ft_push_img(c4, 'P', 0, 0))
+		return (1);
 	(*c4)->p_pos = 0;
 	i = 1;
 	while (i < (*c4)->x)
@@ -38,7 +40,8 @@ int	ft_init_img(t_c4 **c4)
 		j = 0;
 		while (j < (*c4)->y)
 		{
-			ft_push_img(c4, ' ', j, i);
+			if (ft_push_img(c4, ' ', j, i))
+				return (1);
 			j++;
 		}
 		i++;
